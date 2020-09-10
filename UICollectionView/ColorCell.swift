7ca295761a2ce861aa0a29c521eedc31cf99c599
junhyuk <dejavukk1@gmail.com>
2020-09-10 23:07:30 +0900

    뷰컨트롤러 및 커스텀셀 추가.

---
+++ UICollectionView/ColorCell.swift
@@ -0,0 +1,18 @@
+//
+//  ColorCell.swift
+//  UICollectionView
+//
+//  Created by junhyuk on 2020/09/10.
+//  Copyright © 2020 junhyuk. All rights reserved.
+//
+
+import UIKit
+
+class ColorCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
+}
---
+++ UICollectionView/ColorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ColorViewController.swift
+//  UICollectionView
+//
+//  Created by junhyuk on 2020/09/10.
+//  Copyright © 2020 junhyuk. All rights reserved.
+//
+
+import UIKit
+
+class ColorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
